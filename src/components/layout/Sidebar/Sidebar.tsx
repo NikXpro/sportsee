1579fea2e679e6ef3ef4a661c2d2a4009998d93b
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-14 20:49:51 +0100

    feat(Sidebar): add Sidebar component with styling and icons - implement Sidebar component to enhance layout - include icons for user interaction and copyright information - create dedicated SCSS file for Sidebar styles

---
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,22 @@
+import "./Sidebar.scss";
+
+type SidebarProps = {
+  children: React.ReactNode;
+};
+
+export function Sidebar({ children }: SidebarProps) {
+  return (
+    <main className="sidebar-container">
+      <aside className="sidebar">
+        <div className="icons">
+          <img src="/icons/zen.png" alt="zen icon" />
+          <img src="/icons/swim.png" alt="swim icon" />
+          <img src="/icons/becycle.png" alt="bike icon" />
+          <img src="/icons/muscle.png" alt="muscle icon" />
+        </div>
+        <small className="copyright">Copght, SportSee 2020</small>
+      </aside>
+      {children}
+    </main>
+  );
+}
