05e75776fccabac0b220060606487234062733ce
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-14 15:00:04 +0100

    feat(vite): enhance module resolution with path aliases - added path aliases for easier imports throughout the project - improves code readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@providers": path.resolve(__dirname, "src/providers"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@shared": path.resolve(__dirname, "src/components/shared"),
+      "@ui": path.resolve(__dirname, "src/components/ui"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@public": path.resolve(__dirname, "public"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
