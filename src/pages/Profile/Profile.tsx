b268e6f281ca898573849477bd3f7a3cbd0d1162
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-15 15:11:27 +0100

    feat(profile): add profile page layout and styles - create Profile.scss for styling the profile component - implement responsive design and structured layout - add InfoItem component for displaying nutritional data

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,10 +1,47 @@
 import { Layout } from "@components/layout";
+import { InfoItem } from "@pages/Profile/_components/InfoItem";
+import "./Profile.scss";
 
 export function Profile() {
   return (
     <>
       <Layout pageActive="profile">
-        <h1>Profile</h1>
+        <div className="profile-container">
+          <div className="profile-header">
+            <h1>
+              Bonjour <span className="highlight">Thomas</span>
+            </h1>
+            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+          </div>
+          <div className="profile-content">
+            <div className="profile-content-left">
+              <div className="activity-chart"></div>
+              <div className="activity-stats"></div>
+            </div>
+            <div className="profile-content-right">
+              <InfoItem
+                icon={"/icons/calories-icon.png"}
+                title={"Calories"}
+                value={"150kcal"}
+              />
+              <InfoItem
+                icon={"/icons/protein-icon.png"}
+                title={"Proteines"}
+                value={"155g"}
+              />
+              <InfoItem
+                icon={"/icons/carbs-icon.png"}
+                title={"Glucides"}
+                value={"290g"}
+              />
+              <InfoItem
+                icon={"/icons/fat-icon.png"}
+                title={"Lipides"}
+                value={"50g"}
+              />
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
