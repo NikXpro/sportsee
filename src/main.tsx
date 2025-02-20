a1adac1e7675c09174d2b467d0baba61e7492f12
NikX <41104633+NikXpro@users.noreply.github.com>
2025-02-20 15:37:16 +0100

    feat(main): update routing to include Home and dynamic profile - replace Profile component with Home at root path - add dynamic route for Profile with an ID parameter - include a fallback route to navigate to the Home component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
+import { Home } from "@pages/Home";
 import { Profile } from "@pages/Profile";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
