0b228aad830483853faaf9a9837407ae682b9bff
NikX <41104633+NikXpro@users.noreply.github.com>
2025-02-20 15:37:58 +0100

    feat(config): add API configuration file - create API_CONFIG to centralize API base URL and endpoints - improve maintainability and readability of API-related settings

---
+++ src/config/api.config.ts
@@ -0,0 +1,9 @@
+export const API_CONFIG = {
+  BASE_URL: "http://localhost:3000",
+  ENDPOINTS: {
+    USER: "/user/:id",
+    USER_ACTIVITY: "/user/:id/activity",
+    USER_AVERAGE_SESSIONS: "/user/:id/average-sessions",
+    USER_PERFORMANCE: "/user/:id/performance",
+  },
+};
