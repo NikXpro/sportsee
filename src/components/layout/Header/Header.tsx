adb1df9cb3436fc4e969f0279e6ac11a26729f91
NikX <41104633+NikXpro@users.noreply.github.com>
2025-02-20 15:20:42 +0100

    fix(Header): correct import and logo path - change import from 'react-router' to 'react-router-dom' for proper routing - update logo image source path to start from root for consistency

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { navList } from "@/main.const";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 export function Header({ pageActive }: { pageActive: string }) {
   return (
     <>
       <header className="header">
-        <img className="logo" src={"logo.png"} alt="logo" />
+        <img className="logo" src="/logo.png" alt="logo" />
         <nav className="nav">
           {navList.map((navitem) => (
             <Link
